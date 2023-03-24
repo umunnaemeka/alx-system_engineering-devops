ef3080f3517d89a6ad9e37c365cb266cd951ffa2
umunnaemeka <umunnaemeka@gmail.com>
2023-03-24 00:26:34 -0700

    config Management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+
+# Creates a file in /tmp
+
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
